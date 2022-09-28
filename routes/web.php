f8a288c0275e7e24125354e1546fc4e661d48a67
MohamedGamalHG <49466286+MohamedGamalHG@users.noreply.github.com>
2022-09-28 23:45:05 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes(['register'=>false]);
 
@@ -42,7 +42,7 @@ Route::group([
 
 Route::group([
             'namespace'     => 'App\Http\Controllers\Customer',
-            'prefix'        =>  'HomePage'
+            'prefix'        =>  ''
             ],function (){
 
 Route::resource('/','HomePageController');
